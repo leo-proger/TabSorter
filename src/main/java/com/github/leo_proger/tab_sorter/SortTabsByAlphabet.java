e400848fbdf437441129585ef9231ba2ddf190c5
Leo Proger <leoproger11@gmail.com>
2025-08-20 18:30:57 +0300

    refactor: move all non-sorting features to the basic class

--- src/main/java/com/github/leo_proger/tab_sorter/SortTabsByAlphabet.java
+++ src/main/java/com/github/leo_proger/tab_sorter/SortTabsByAlphabet.java
@@ -2,11 +2,6 @@ package com.github.leo_proger.tab_sorter;
 
 
 import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.actionSystem.CommonDataKeys;
-import com.intellij.openapi.fileEditor.FileEditorManager;
-import com.intellij.openapi.fileEditor.impl.EditorWindow;
-import com.intellij.openapi.fileEditor.impl.FileEditorManagerImpl;
-import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
 import org.jetbrains.annotations.NotNull;
 
@@ -18,25 +13,12 @@ public class SortTabsByAlphabet extends Sorter {
 
 	@Override
 	public void actionPerformed(@NotNull AnActionEvent e) {
-		Project project = e.getProject();
-		if (project == null) return;
+		List<VirtualFile> openFiles = getOpenFiles(e);
+		if (openFiles == null) return;
 
-		VirtualFile clickedFile = e.getData(CommonDataKeys.VIRTUAL_FILE);
-		if (clickedFile == null) return;
+		openFiles.sort(Comparator.comparing(VirtualFile::getName, String.CASE_INSENSITIVE_ORDER));
 
-		FileEditorManager fileEditorManager = FileEditorManager.getInstance(project);
-		if (!(fileEditorManager instanceof FileEditorManagerImpl manager)) return;
-
-
-		EditorWindow targetWindow = findWindowContainingFile(manager, clickedFile);
-		if (targetWindow == null) return;
-
-		List<VirtualFile> files = targetWindow.getFileList();
-		if (files.size() < 2) return;
-
-		files.sort(Comparator.comparing(VirtualFile::getName, String.CASE_INSENSITIVE_ORDER));
-
-		reorderTabs(targetWindow, files);
+		reorderTabs(findWindowContainingFile(e), openFiles);
 	}
 
 }
--- src/main/java/com/github/leo_proger/tab_sorter/Sorter.java
+++ src/main/java/com/github/leo_proger/tab_sorter/Sorter.java
@@ -2,8 +2,12 @@ package com.github.leo_proger.tab_sorter;
 
 
 import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.fileEditor.FileEditorManager;
 import com.intellij.openapi.fileEditor.impl.EditorWindow;
 import com.intellij.openapi.fileEditor.impl.FileEditorManagerImpl;
+import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
 
 import java.util.List;
@@ -11,20 +15,21 @@ import java.util.List;
 
 abstract public class Sorter extends AnAction {
 
-	protected void reorderTabs(EditorWindow window, List<VirtualFile> files) {
+	protected void reorderTabs(EditorWindow window, List<VirtualFile> sortedFiles) {
 		List<VirtualFile> currentFiles = window.getFileList();
 		for (VirtualFile file : currentFiles)
 		{
 			window.closeFile(file, false, false);
 		}
 
-		for (VirtualFile file : files)
+		for (VirtualFile file : sortedFiles)
 		{
 			window.getManager().openFileImpl2(window, file, true);
 		}
 	}
 
-	protected EditorWindow findWindowContainingFile(FileEditorManagerImpl manager, VirtualFile clickedFile) {
+	protected EditorWindow findWindowContainingFile(AnActionEvent e) {
+		FileEditorManagerImpl manager = getFileEditorManager(e);
 		EditorWindow[] windows = manager.getWindows();
 
 		for (EditorWindow window : windows)
@@ -32,13 +37,44 @@ abstract public class Sorter extends AnAction {
 			List<VirtualFile> files = window.getFileList();
 			for (VirtualFile file : files)
 			{
-				if (file.equals(clickedFile))
+				if (file.equals(getClickedFile(e)))
 				{
 					return window;
 				}
 			}
 		}
 		return null;
+	}
+
+	protected List<VirtualFile> getOpenFiles(AnActionEvent e) {
+		Project project = getProject(e);
+		if (project == null) return null;
+
+		FileEditorManagerImpl manager = getFileEditorManager(e);
+		if (manager == null) return null;
+
+		VirtualFile clickedFile = getClickedFile(e);
+		if (clickedFile == null) return null;
+
+		EditorWindow targetWindow = findWindowContainingFile(e);
+		if (targetWindow == null) return null;
+
+		List<VirtualFile> openFiles = targetWindow.getFileList();
+		if (openFiles.size() < 2) return null;
+
+		return openFiles;
+	}
+
+	protected VirtualFile getClickedFile(AnActionEvent e) {
+		return e.getData(CommonDataKeys.VIRTUAL_FILE);
+	}
+
+	protected Project getProject(AnActionEvent e) {
+		return e.getProject();
+	}
+
+	protected FileEditorManagerImpl getFileEditorManager(AnActionEvent e) {
+		return (FileEditorManagerImpl) FileEditorManager.getInstance(getProject(e));
 	}
 
 }
